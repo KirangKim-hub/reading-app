61865343f3b2ea8e1e5037c4f50743cebfb9739b
KirangKim-hub <caligula0410@gmail.com>
2026-05-21 00:04:58 +0900

    Update serve_new.js

--- serve_new.js
+++ serve_new.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = async function handler(req, res) {
-  const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+  const filePath = path.join(process.cwd(), 'index.html');
+  const html = fs.readFileSync(filePath, 'utf8');
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Cache-Control', 'no-store');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
   res.status(200).end(html);
 };
