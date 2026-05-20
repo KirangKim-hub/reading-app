3cb8cf95b01c47b2ea5b88212aa5928b67d337ee
KirangKim-hub <caligula0410@gmail.com>
2026-05-20 19:01:09 +0900

    Update items.js

--- api/items.js
+++ api/items.js
@@ -6,10 +6,10 @@ const supabase = createClient(
 );
 
 module.exports = async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  if (req.method === 'OPTIONS') return res.status(200).end();
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+ res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+ res.setHeader('Cache-Control', 'no-store');
 
   try {
     if (req.method === 'GET') {
